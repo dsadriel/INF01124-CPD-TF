571592ce39d8ac73fb5426686302af56fdb01945
dsadriel <ds.adriel@gmail.com>
2024-08-18 18:23:02 -0300

    added missing headers

--- include/entidades_utils.h
+++ include/entidades_utils.h
@@ -10,6 +10,10 @@ void imprimir(TipoEntidade tipo, void* entidade);
 void __imprimir_paciente(Paciente* paciente);
 void __imprimir_medico(Medico* medico);
 void __imprimir_agendamento(Agendamento* agendamento);
+void imprimir_linha(TipoEntidade tipo, void* entidade);
+void __imprimir_paciente_linha(Paciente* paciente);
+void __imprimir_medico_linha(Medico* medico);
+void __imprimir_agendamento_linha(Agendamento* agendamento);
 Timestamp obter_data_hora_atual();
 
 void ler_entidade(TipoEntidade tipo, void *entidade);
